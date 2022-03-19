0c5342c4e91e679e5539e9cf06eccd4a937f0ab8
OumaimaHF <oumaimahafid30@gmail.com>
2022-03-19 08:49:53 +0100

    Tp11

---
+++ src/main/java/cigma/pfe/services/FactureService.java
@@ -0,0 +1,17 @@
+package cigma.pfe.services;
+
+
+import cigma.pfe.models.Facture;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface FactureService {
+
+    public Facture save(Facture f);
+    public Facture modify(Facture f);
+    public List<Facture> getAll();
+    public Facture getOne(long id);
+    public void remove(long id);
+    public List<Facture> Find(LocalDate date);
+}
